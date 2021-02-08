1de9b57f75deafc3043fcbd9bd10ae72b0b808e5
tomas Salas <tomassubero97@gmail.com>
2021-02-08 14:36:38 -0600

    0.2

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ const selectorOptionOther = document.querySelector("#other-job-role");
 selectorOptionOther.setAttribute("type", "hidden");
 
 const selectJob = document.querySelector("#title");
-const options = document.querySelectorAll("#title option")
+const jobOptions = document.querySelectorAll("#title option")
 
 selectJob.addEventListener("change", event => {
         console.log(event.target.value);
-        console.log(options[6]);
+        console.log(jobOptions[6]);
         if (event.target.value === options[6].value) {
             selectorOptionOther.setAttribute("type", "text");
         } else {
@@ -22,3 +22,46 @@ selectJob.addEventListener("change", event => {
 
 //step 5 
 
+const selectDesign = document.querySelector("#design");
+const designOptions = document.querySelectorAll("#design option")
+const selectColor = document.querySelector("#shirt-colors")
+const shirtOptions = document.querySelectorAll("#color option")
+selectColor.style.visibility = "hidden";
+
+
+
+
+
+
+selectDesign.addEventListener("change", event => {
+    if (event.target.value === designOptions[1].value) {
+        let shirtType = event.target
+        selectColor.style.visibility = "visible";
+        for (let i = 1; i < shirtOptions.length; i += 1) { 
+            let shirtTypeOptions = shirtOptions[i].getAttribute("data-theme");
+            if ( shirtTypeOptions === shirtType.value  ) {
+                shirtOptions[i].style.display = "inline";
+            } else {
+                shirtOptions[i].style.display = "none";
+            }
+    
+        } 
+    } 
+
+    if (event.target.value === designOptions[1].value) {
+        let shirtType = event.target
+        selectColor.style.visibility = "visible";
+        for (let i = 1; i < shirtOptions.length; i += 1) { 
+            let shirtTypeOptions = shirtOptions[i].getAttribute("data-theme");
+            if ( shirtTypeOptions === shirtType.value  ) {
+                shirtOptions[i].style.display = "inline";
+            } else {
+                shirtOptions[i].style.display = "none";
+            }
+    
+        } 
+    } 
+});
+
+
+// step 6
