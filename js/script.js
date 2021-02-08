6a396e3b2a55ffe2a702d2860cdc08c56a37589a
tomas Salas <tomassubero97@gmail.com>
2021-02-07 18:08:49 -0600

    0.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+
+// step one
+const inputName = document.querySelector("#name");
+inputName.focus();
+
+//step two
+const selectorOptionOther = document.querySelector("#other-job-role");
+selectorOptionOther.setAttribute("type", "hidden");
+
+const selectJob = document.querySelector("#title");
+const options = document.querySelectorAll("#title option")
+
+selectJob.addEventListener("change", event => {
+        console.log(event.target.value);
+        console.log(options[6]);
+        if (event.target.value === options[6].value) {
+            selectorOptionOther.setAttribute("type", "text");
+        } else {
+            selectorOptionOther.setAttribute("type", "hidden");
+        }
+});
+
+//step 5 
+
